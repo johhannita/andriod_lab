163d5d4738ace7d162f43746da4bebf59209060c
Eva Johanna Rigmanyi <99803378+j-rigmanyi@users.noreply.github.com>
2023-10-15 23:52:29 +0300

    majdnem kesz

--- kotlin-exercises/labor-02/src/main/Date.kt
+++ kotlin-exercises/labor-02/src/main/Date.kt
@@ -1,23 +1,18 @@
 package main
 
 import java.util.*
+import java.util.Date
 
 data class Date(var year: Int = Calendar.YEAR,
                 var month: Int = Calendar.MONTH,
-                var day: Int = Calendar.DAY_OF_MONTH): Comparable<Date> {
-    companion object: Comparable<Date> {
-//        override fun compareTo(a: Date, b: Date): Int = when {
-//            a.year != b.year -> a.year - b.year
-//        }
-
-        override fun compareTo(a: Date, b: Date): Int = when {
-            a.year != b.year -> a.year - b.year
+                var day: Int = Calendar.DAY_OF_MONTH) : Comparable<Date> {
+    override fun compareTo(other: Date): Int {
+        return when {
+            year != other.year -> year - other.year
+            month != other.month -> month - other.month
+            else -> day - other.day
         }
     }
 
-    override fun compareTo(other: Date): Int {
-        TODO("Not yet implemented")
-    }
-}
-
+                 }
 
